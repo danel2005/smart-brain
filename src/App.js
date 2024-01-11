72a2c5a030a0f3e772b5acecb7d27a73802bb511
danel2005 <dcoolmineduck@gmail.com>
2024-01-11 20:31:12 +0200

    chagne lyric

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React olga
         </a>
       </header>
     </div>
